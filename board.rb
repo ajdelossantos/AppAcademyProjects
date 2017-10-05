5f7f4fce8f84d045b007d21671e022a3843d9992
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-10-05 15:50:30 -0700

    add populate_numbers

--- board.rb
+++ board.rb
@@ -3,10 +3,11 @@ require_relative 'tile'
 class Board
   def initialize()
     @grid = Array.new(9) { Array.new(9) { Tile.new(0) } }
-    populate
+    populate_bombs
+    populate_numbers
   end
 
-  def populate
+  def populate_bombs
     bombs_number = 10
     positions = []
     (0...@grid.length).each do |i|
@@ -36,6 +37,33 @@ class Board
       puts
     end
   end
+
+  def pos_in_range?(pos)
+    a,b = pos
+    (0...@grid.length).cover?(a) && (0...@grid.length).cover?(b)
+  end
+
+  def populate_numbers
+    # check neighbor's value
+    (0...@grid.length).each do |i|
+      (0...@grid[0].length).each do |j|
+
+        unless self[[i,j]].is_bomb?
+          adjacent_move =
+            [-1, 0, 1].permutation(2).to_a + [[-1, -1], [1, 1]]
+
+          bombs = adjacent_move.select do |m|
+            pos_in_range?([i+m[0], j+m[1]]) && self[[i+m[0], j+m[1]]].is_bomb?
+          end
+
+          self[[i,j]].value = bombs.length
+        end
+      end
+    end
+
+
+  end
+
 end
 
 b = Board.new
--- tile.rb
+++ tile.rb
@@ -1,12 +1,14 @@
 class Tile
   attr_accessor :value
-  
+
   def initialize(value)
     @value = value
     @face_up = false
     @flagged = false
   end
 
-
+  def is_bomb?
+    @value == '⚽️'
+  end
 
 end
