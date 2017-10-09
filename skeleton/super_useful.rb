92720a8025f779eecf64aefe0080d181b014c8e8
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-10-09 14:38:21 -0700

    complete exercise one

--- skeleton/super_useful.rb
+++ skeleton/super_useful.rb
@@ -1,17 +1,25 @@
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+  begin
+    Integer(str)
+  rescue ArgumentError
+    puts 'Cannot convert input to Integer'
+  end
 end
 
 # PHASE 3
-FRUITS = ["apple", "banana", "orange"]
+FRUITS = ["coffee"]
 
 def reaction(maybe_fruit)
-  if FRUITS.include? maybe_fruit
-    puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
-    raise StandardError 
-  end 
+  begin
+    raise StandardError unless FRUITS.include? maybe_fruit
+    rescue StandardError
+      puts "feed me coffee"
+      maybe_fruit = gets.chomp
+  retry
+ensure
+  puts "thank you"
+  end
 end
 
 def feed_me_a_fruit
@@ -19,12 +27,33 @@ def feed_me_a_fruit
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
-end  
+  reaction(maybe_fruit)
+end
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError if yrs_known < 5 || name == "" || fav_pastime == ""
+
+  rescue ArgumentError
+    if yrs_known < 5
+      puts "A friend of less than 5 years is no BEST friend"
+      yrs_known = gets.chomp.to_i
+      retry
+
+    elsif name == ""
+      puts "Your best friend needs a name."
+      name = gets.chomp
+      retry
+
+    elsif fav_pastime == ""
+      puts "Wait, you do what together?"
+      fav_pastime = gets.chomp
+      retry
+    end
+
+  ensure
+    puts "Now THAT'S a bestie."
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
@@ -39,8 +68,6 @@ class BestFriend
   end
 
   def give_friendship_bracelet
-    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me."
   end
 end
-
-
