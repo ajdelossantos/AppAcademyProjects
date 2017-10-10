3a2a5c8b3f675d10d2b503872acda232777bbf61
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-10-09 17:55:05 -0700

    Progress on board.rb methods populate and valid_move?

--- Chess/board.rb
+++ Chess/board.rb
@@ -1,12 +1,22 @@
-require 'piece'
+require_relative 'piece'
+require_relative 'display'
 class Board
 
   def initialize
     @grid = Array.new(8) { Array.new(8) }
-    # @grid.each { |row| row.each { |el| el = NullPiece.instance } }
+    self.populate
   end
 
-  def []=(pos, piece)
+  def populate
+    @grid.each_with_index do |row, x|
+      row.each_index do |y|
+        pos = [x, y]
+        self[pos] = NullPiece.instance
+      end
+    end
+  end
+
+  def []=(pos, piece = board[pos[0], pos[1]])
     x, y = pos
     @grid[x][y] = piece
   end
@@ -16,11 +26,29 @@ class Board
     @grid[x][y]
   end
 
-  def move_piece(start_pos, end_pos)
+  def move_piece!(start_pos, end_pos)
     s_x, s_y = start_pos
     e_x, e_y = end_pos
-    raise StandardError
-    @grid[e_x][e_y] = @grid[s_x][s_y]
-    @grid[s_x][s_y] = NullPiece.new
+
+    self[e_x][e_y] = self[s_x][s_y]
+    self[s_x][s_y] = NullPiece.instance
+  end
+
+  def move_piece(color, start_pos, end_pos)
+    raise StandardError unless valid_move?(color, start_pos, end_pos)
+  rescue StandardError
+    puts "Illegal move, please try again"
+    #TODO add retry until we get a legal move
+    # if legal...
+  ensure
+    move_piece!(start_pos, end_pos)
+  end
+
+  def valid_move?(color, start_pos, end_pos)
+    #TODO logic when in "check"
+    return false if self[start_pos].empty?
+    return false if self[end_pos].color == color
+    return false unless self[start_pos].valid_moves.include?(end_pos)
+    true
   end
 end
--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,24 +1,38 @@
-# require board?
+require 'singleton'
+
 class Piece
-  def initialize(color = nil, type = nil)
+  def initialize(pos, color = nil, type = nil)
     @color = color
     @type = type
+    @pos = pos
   end
 
   def to_s
+    # Delimit by " "
+    "#{@color} #{@type}"
   end
 
-  def valid_moves()
+  def empty?
+    type.nil?
+  end
+
+  def valid_moves
+    # Array of positions
+    []
   end
 end
 
 class NullPiece < Piece
-
+  include Singleton
+  def initialize(pos = nil, color = nil, type = nil)
+    super
+  end
 end
 
 # For Example...
 class King < Piece
-  def initialize(color = nil, type = nil)
+  # TODO Include moduleStepable
+  def initialize(pos, color = nil, type = nil)
     super
   end
 end
