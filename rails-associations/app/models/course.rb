59b66d86037f6a29db302ee4d239045d688a1288
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-10-18 14:32:55 -0700

    creates active record associations for course, user, and enrollment models

--- rails-associations/app/models/course.rb
+++ rails-associations/app/models/course.rb
@@ -1,3 +1,31 @@
 class Course < ApplicationRecord
-  
+
+  has_many(
+    :enrollments,
+    class_name: 'Enrollment',
+    foreign_key: :course_id,
+    primary_key: :id
+  )
+
+  has_many(
+    :students,
+    through: :enrollments,
+    source: :users
+  )
+
+  belongs_to(
+    :prereq,
+    class_name: 'Course',
+    foreign_key: :prereq_id,
+    primary_key: :id,
+    optional: true
+  )
+
+  belongs_to(
+    :instructor,
+    class_name: 'User',
+    foreign_key: :instructor_id,
+    primary_key: :id
+  )
+
 end
--- rails-associations/app/models/user.rb
+++ rails-associations/app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
-  
+  validates :name, presence: true
+
+  has_many(
+    :enrollments,
+    class_name: 'Enrollment',
+    foreign_key: :student_id,
+    primary_key: :id
+  )
+
+  has_many(
+    :courses,
+    through: :enrollments,
+    source: :courses
+  )
+
+
+
 end
