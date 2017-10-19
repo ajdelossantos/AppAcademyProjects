1bcc43a98a30d9e0c57ff7cdfbea510979f4cd1f
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-10-19 16:56:06 -0700

    Work on 02

--- W3D4/movie_buff/movie_buff/02_queries.rb
+++ W3D4/movie_buff/movie_buff/02_queries.rb
@@ -2,18 +2,28 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where(score: 3..5)
+    .where(yr: 1980..1989)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie
+    .group(:yr)
+    .having('MAX(score) < 8')
+    .pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-
+  Actor
+    .select(:id, :name)
+    .joins(:movies)
+    .where('movies.title = ?', title)
+    .order('castings.ord ASC')
 end
 
 def vanity_projects
@@ -22,7 +32,11 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
-
+  Movie
+    .select('movies.id, movies.title, actors.name')
+    .joins(:actors)
+    .where('castings.actor_id = movies.director_id')
+    .where('castings.ord = 1')
 end
 
 def most_supportive
