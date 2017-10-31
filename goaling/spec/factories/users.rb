40bec895d12cb777d527e17e1b0a719ff0e36f94
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-10-30 18:09:54 -0700

    namespace W5D1 work

--- goaling/spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    username { Faker::Internet.user_name }
-    password { Faker::Internet.password }
-    # password_digest "MyString"
-    # session_token "MyString"
-  end
-end
