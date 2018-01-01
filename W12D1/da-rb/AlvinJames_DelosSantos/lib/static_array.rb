f00bc155c52c83a57c7e04a180e32c61fcc70de7
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-12-31 18:38:06 -0800

    Finish Static Array Class

--- W12D1/da-rb/AlvinJames_DelosSantos/lib/static_array.rb
+++ W12D1/da-rb/AlvinJames_DelosSantos/lib/static_array.rb
@@ -1,14 +1,18 @@
 # This class just dumbs down a regular Array to be statically sized.
 class StaticArray
   def initialize(length)
-    @length = length
+    self.store = Array.new(length)
   end
 
   # O(1)
-  def [](index); end
+  def [](index)
+    store[index]
+  end
 
   # O(1)
-  def []=(index, value); end
+  def []=(index, value)
+    store[index] = value
+  end
 
   protected
 
