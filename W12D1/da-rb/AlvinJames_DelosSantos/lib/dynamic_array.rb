ffd38ca66cdad3948d3191bce8bf1b5f12644eba
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-01-02 11:33:57 -0800

    Start Ring Buffer

--- W12D1/da-rb/AlvinJames_DelosSantos/lib/dynamic_array.rb
+++ W12D1/da-rb/AlvinJames_DelosSantos/lib/dynamic_array.rb
@@ -4,41 +4,107 @@ class DynamicArray
   attr_reader :length
 
   def initialize
+    @store = StaticArray.new(8)
+    @capacity = 8
+    @length = 0
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    check_index(index)
+    @store[index] = value
   end
-
+  
   # O(1)
   def pop
+    check_length(@length)
+    last_idx = @length - 1
+    last = @store[last_idx]
+
+    @store[last_idx] = nil
+    @length -= 1
+    
+    last
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    resize! if @length == @capacity
+    
+    @store[@length] = val
+    @length += 1
+    
+    @store
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    check_length(@length)
+    
+    first = @store[0]
+    temp_arr = StaticArray.new(@capacity)
+
+    i = 1
+    while i < @length
+      temp_arr[i - 1] = @store[i]
+      i += 1
+    end
+
+    @store = temp_arr
+    @length -= 1
+
+    first
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    resize! if @length == @capacity
+    
+    temp_arr = @store
+    @store = StaticArray.new(@capacity)
+    @store[0] = val
+
+    i = 0
+    while i < @length
+      @store[i + 1] = temp_arr[i]
+      i += 1
+    end
+
+    @length += 1
+    @store
   end
 
   protected
+
   attr_accessor :capacity, :store
   attr_writer :length
 
   def check_index(index)
+    raise "index out of bounds" if index >= @length
+  end
+
+  def check_length(length)
+    raise "index out of bounds" if @length == 0
   end
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    temp_arr = @store
+    @capacity *= 2
+    @store = StaticArray.new(@capacity)
+
+    i = 0
+    while i < @length
+      @store[i] = temp_arr[i]
+      i += 1
+    end
+
   end
 end
--- W12D1/da-rb/AlvinJames_DelosSantos/lib/ring_buffer.rb
+++ W12D1/da-rb/AlvinJames_DelosSantos/lib/ring_buffer.rb
@@ -4,14 +4,22 @@ class RingBuffer
   attr_reader :length
 
   def initialize
+    @store = StaticArray.new(8)
+    @capacity = 8
+    @length = 0
+    @start_idx = 0
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    @store[index]
   end
 
   # O(1)
-  def []=(index, val)
+  def []=(index, value)
+    check_index(index)
+    @store[index] = value
   end
 
   # O(1)
@@ -35,8 +43,23 @@ class RingBuffer
   attr_writer :length
 
   def check_index(index)
+    raise "index out of bounds" if index >= @length
+  end
+
+  def check_length(length)
+    raise "index out of bounds" if @length == 0
   end
 
   def resize!
+    temp_arr = @store
+    @capacity *= 2
+    @store = StaticArray.new(@capacity)
+
+    i = 0
+    while i < @length
+      @store[i] = temp_arr[i]
+      i += 1
+    end
+    @start_idx = 0
   end
 end
