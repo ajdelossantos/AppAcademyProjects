ccb384e5317f13f5ff9edea3bec2ca4aa456949b
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-01-03 10:50:36 -0800

    Finish LRU project

--- W12D2/hash-map-LRU/AlvinJames_DelosSantos/lib/p07_practical.rb
+++ W12D2/hash-map-LRU/AlvinJames_DelosSantos/lib/p07_practical.rb
@@ -1,5 +1,11 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  count = HashMap.new
+  string.chars.each { |letter| count.include?(letter) ? count[letter] += 1 : count[letter] = 1 }
+
+  odds = 0
+  count.each { |_letter, count| odds += 1 if count.odd? }
+
+  odds > 1 ? false : true
 end
