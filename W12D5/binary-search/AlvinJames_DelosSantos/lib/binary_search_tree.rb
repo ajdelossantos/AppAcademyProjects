7367fcdefed6b6ef646f8465a6d504212d44af61
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-01-08 16:33:23 -0800

    Finish BST

--- W12D5/binary-search/AlvinJames_DelosSantos/lib/binary_search_tree.rb
+++ W12D5/binary-search/AlvinJames_DelosSantos/lib/binary_search_tree.rb
@@ -13,29 +13,40 @@ class BinarySearchTree
     new_node = BSTNode.new(value)
     if @root.nil?
       @root = new_node
-      return true
     else
       @root.add_val(new_node, value)
     end
   end
 
   def find(value, tree_node = @root)
+    @root.nil? ? nil : tree_node.find_val(value)
   end
 
   def delete(value)
+    if @root.nil?
+      nil
+    elsif @root.left.nil? && @root.right.nil?
+      @root = nil
+    else
+      @root.remove_val(value)
+    end
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    @root.nil? ? nil : tree_node.get_max
   end
 
   def depth(tree_node = @root)
+    @root.nil? ? 0 : tree_node.get_depth
   end 
 
   def is_balanced?(tree_node = @root)
+    tree_node.check_balance
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
+    tree_node.traverse
   end
 
 
