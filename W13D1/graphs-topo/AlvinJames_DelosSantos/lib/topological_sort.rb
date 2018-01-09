6d048c888941dee64a37ee762f39353d926d43e0
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-01-08 21:03:02 -0800

    Pseudo-coded Topo sort

--- W13D1/graphs-topo/AlvinJames_DelosSantos/lib/topological_sort.rb
+++ W13D1/graphs-topo/AlvinJames_DelosSantos/lib/topological_sort.rb
@@ -3,5 +3,32 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  #beware Array#each
+  # queue up all vertices w/ n in-edges
+  # pop off vertices from queue
+    # Remove vertex + out_edges from graph
+    # Push vertices into sorted array
+    # Examine destination vertices, push onto queue if no more in-edges
+  sorted = []
+  top = [] # a queue
 
+  vertices.each do |vertex|
+    if vertex.in_edges.empty?
+      top.unshift(vertex)
+    end
+  end
+
+  until top.empty?
+    current = top.pop
+    sorted << current
+
+    current.out_edges.each do |edge|
+      if edge.to_vertex.in_edges.empty?
+        top.unshift(edge)
+      end
+      vertices.delete(edge)
+    end
+  sorted
+  end
 end
+
